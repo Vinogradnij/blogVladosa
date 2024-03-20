3dcb677a8e9ac8927065ba43a0a87f9541955f03
Vinogradnij <Vlud5@mail.ru>
2024-03-20 16:58:57 +0300

    Созданы модели-заглушки, представления-заглушки

--- blogvladosa/blog/views.py
+++ blogvladosa/blog/views.py
@@ -1,3 +1,27 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse('HomePage')
+
+
+def detail(request):
+    return HttpResponse('DetailPage')
+
+
+def create(request):
+    return HttpResponse('CreatePage')
+
+
+def archive(request):
+    return HttpResponse('ArchivePage')
+
+
+def login(request):
+    return HttpResponse('LoginPage')
+
+
+def logout(request):
+    return HttpResponse('LogoutPage')
