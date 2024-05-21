e9a1ac4c93c220a4de1f03901f2b23bcc37b7761
Vinogradnij <Vlud5@mail.ru>
2024-05-21 12:00:38 +0300

    Реализация пагинации страниц

--- blogvladosa/blog/utils.py
+++ blogvladosa/blog/utils.py
@@ -2,6 +2,8 @@ from . import models
 
 
 class DataMixin:
+    paginate_by = 6
+
     def get_mixin_context(self, context, **kwargs):
         context['categories'] = models.Category.objects.all()
         context.update(kwargs)
