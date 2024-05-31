c54ada6f0eb62c607cc33ae33c765bed2b36de2a
Vinogradnij <Vlud5@mail.ru>
2024-05-31 20:56:53 +0300

    Реализована регистрация пользователей

--- blogvladosa/users/urls.py
+++ blogvladosa/users/urls.py
@@ -7,4 +7,5 @@ app_name = 'users'
 urlpatterns = [
     path('login/', views.UsersLogin.as_view(), name='login'),
     path('logout/', views.logout_user, name='logout'),
+    path('register/', views.UsersRegister.as_view(), name='register'),
 ]
--- blogvladosa/users/views.py
+++ blogvladosa/users/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.contrib.auth import logout
+from django.views import generic
 import django.contrib.auth.views as auth_views
+
+from . import forms
 
 
 class UsersLogin(auth_views.LoginView):
@@ -13,3 +16,11 @@ class UsersLogin(auth_views.LoginView):
 def logout_user(request):
     logout(request)
     return HttpResponseRedirect(reverse('users:login'))
+
+
+class UsersRegister(generic.CreateView):
+    form_class = forms.RegisterUserForm
+    template_name = 'users/register.html'
+    extra_context = {'title': 'Регистрация', 'content_name': 'Регистрация'}
+
+    success_url = reverse_lazy('users:login')
