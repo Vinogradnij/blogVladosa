1249fe46cbcedfa9922e22c103998396632b6a46
Vinogradnij <Vlud5@mail.ru>
2024-05-31 18:02:57 +0300

    Реализована авторизация пользователя

--- blogvladosa/users/views.py
+++ blogvladosa/users/views.py
@@ -1,10 +1,26 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,  HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+
+from . import forms
 
 
-def login(request):
-    return HttpResponse('LoginPage')
+def login_user(request):
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user and user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('blog:home'))
+
+    else:
+        form = forms.LoginForm()
+    return render(request, template_name='users/login.html', context={'form': form})
 
 
-def logout(request):
-    return HttpResponse('LogoutPage')
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('users:login'))
