48be62e01b8e6024fd98044586d727cf8d35d310
Vinogradnij <Vlud5@mail.ru>
2024-05-31 16:26:14 +0300

    Перенос url для страниц login и logout из приложения blog в приложение users

--- blogvladosa/blogvladosa/urls.py
+++ blogvladosa/blogvladosa/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('users/', include('users.urls', namespace='users')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
