49443734efa6097e65c493b8256ed39a0f7b9b14
Vinogradnij <Vlud5@mail.ru>
2024-05-31 18:43:05 +0300

    Изменение представления для страницы авторизации с функции на класс с использованием LoginView

--- blogvladosa/users/views.py
+++ blogvladosa/users/views.py
@@ -1,24 +1,14 @@
+from django.contrib.auth.views import LoginView, LogoutView, AuthenticationForm
 from django.http import HttpResponse,  HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 
-from . import forms
 
-
-def login_user(request):
-    if request.method == 'POST':
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request, username=cd['username'], password=cd['password'])
-            if user and user.is_active:
-                login(request, user)
-                return HttpResponseRedirect(reverse('blog:home'))
-
-    else:
-        form = forms.LoginForm()
-    return render(request, template_name='users/login.html', context={'form': form})
+class UsersLogin(LoginView):
+    form_class = AuthenticationForm
+    template_name = 'users/login.html'
+    extra_context = {'title': 'Авторизация', 'content_name': 'Авторизация'}
 
 
 def logout_user(request):
